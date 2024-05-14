d1bc5d8d611ff7660a51f1708b20879ab0ab5129
OliverHuang03 <160797758+OliverHuang03@users.noreply.github.com>
2024-05-14 14:58:46 +1000

    14/05

--- mode1.py
+++ mode1.py
@@ -1,4 +1,5 @@
 from landsites import Land
+from data_structures.bst import BinarySearchTree
 
 
 class Mode1Navigator:
@@ -11,22 +12,58 @@ class Mode1Navigator:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        self.adventurers = adventurers
+        self.sites_tree = BinarySearchTree()
+
+        for land in sites:
+            self.sites_tree[land.get_guardians()/land.get_gold()] = land
 
     def select_sites(self) -> list[tuple[Land, int]]:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        sites_list = []
+        adventurers_left = self.adventurers
+        for node in self.sites_tree:
+            if adventurers_left == 0:
+                break
+            else:
+                num_adventurers = min(adventurers_left, node.item.get_guardians())
+                sites_list.append((node.item, num_adventurers))
+                adventurers_left -= num_adventurers
+
+        return sites_list
 
     def select_sites_from_adventure_numbers(self, adventure_numbers: list[int]) -> list[float]:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        score_list = []
+        original_num_adventurers = self.adventurers
+
+        for number in adventure_numbers:
+            self.adventurers = number
+            sites_list = self.select_sites()
+            score = 0
+
+            for item in sites_list:
+                land, num_adventurers = item
+                score += self.reward(land.get_gold(), land.get_guardians(), num_adventurers)
+
+            score_list.append(score)
+
+        self.adventurers = original_num_adventurers
+
+        return score_list
 
     def update_site(self, land: Land, new_reward: float, new_guardians: int) -> None:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+
+        land.set_gold(new_reward)
+        land.set_guardians(new_guardians)
+
+    def reward(self, gold: float, guardians: int, adventurers: int) -> float:
+        return min(adventurers*gold/guardians, gold)
+
--- mode2.py
+++ mode2.py
@@ -1,5 +1,5 @@
 from landsites import Land
-
+from data_structures.heap import MaxHeap
 
 class Mode2Navigator:
     """
@@ -11,16 +11,24 @@ class Mode2Navigator:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        self.n_teams = n_teams
+        self.site_priority = MaxHeap()
+        self.sites = []
 
     def add_sites(self, sites: list[Land]) -> None:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        self.sites = self.sites + sites
 
     def simulate_day(self, adventurer_size: int) -> list[tuple[Land | None, int]]:
         """
         Student-TODO: Best/Worst Case
         """
+        raise NotImplementedError()
+
+    def compute_score(self) -> float:
+        raise NotImplementedError()
+    
+    def construct_score_data_structure(self) -> None:
         raise NotImplementedError()
